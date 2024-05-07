bea8b34eaa819d34aa4b4fb72dc5211e4a273e99
kailanyue <yuekailan@163.com>
2024-05-07 20:29:54 +0800

    feature: support get/set, hget/hset/hgetall command

--- src/resp/mod.rs
+++ src/resp/mod.rs
@@ -45,7 +45,8 @@ pub enum RespError {
 }
 
 #[enum_dispatch(RespEncode)]
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+
 pub enum RespFrame {
     SimpleString(SimpleString),
     Error(SimpleError),
@@ -62,32 +63,32 @@ pub enum RespFrame {
 }
 
 // 创建 RespFrame 对应的 Struct
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
-pub struct SimpleString(String);
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub struct SimpleString(pub(crate) String);
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
-pub struct SimpleError(String);
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub struct SimpleError(pub(crate) String);
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
-pub struct BulkString(Vec<u8>);
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub struct BulkString(pub(crate) Vec<u8>);
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
 pub struct RespNullBulkString;
 
-#[derive(Debug, PartialEq, PartialOrd)]
-pub struct RespArray(Vec<RespFrame>);
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct RespArray(pub(crate) Vec<RespFrame>);
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
 pub struct RespNullArray;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
 pub struct RespNull;
 
-#[derive(Debug, PartialEq, PartialOrd)]
-pub struct RespMap(BTreeMap<String, RespFrame>);
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct RespMap(pub(crate) BTreeMap<String, RespFrame>);
 
-#[derive(Debug, PartialEq, PartialOrd)]
-pub struct RespSet(Vec<RespFrame>);
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct RespSet(pub(crate) Vec<RespFrame>);
 
 // 为 Struct 实现 Deref
 impl Deref for SimpleString {
@@ -233,5 +234,43 @@ impl<const N: usize> From<&[u8; N]> for BulkString {
 impl<const N: usize> From<&[u8; N]> for RespFrame {
     fn from(s: &[u8; N]) -> Self {
         BulkString(s.to_vec()).into()
+    }
+}
+
+impl AsRef<str> for SimpleString {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl AsRef<[u8]> for BulkString {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_string_test() {
+        let s = SimpleString::from("hello");
+        assert_eq!(s, "hello".into());
+    }
+
+    #[test]
+    fn simple_error_test() {
+        let s = SimpleError::from("hello");
+        assert_eq!(s, "hello".into());
+    }
+
+    #[test]
+    fn bulk_string_test() {
+        let s = BulkString::from("hello");
+        assert_eq!(s, "hello".into());
+
+        let s = BulkString::from("world".as_bytes());
+        assert_eq!(s, "world".into());
     }
 }
